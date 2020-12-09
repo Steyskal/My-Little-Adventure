604057a865b5d7730be30c434d8d9eb4f6bb48b0
Toni Steyskal <toni.steyskal@gmail.com>
2020-12-09 21:05:45 +0100

    Predavanja 09.12.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 5.0f;
+
+    [Header("Read-only")]
+    [SerializeField] private Vector2 _destination = Vector2.zero;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        Vector2 movementDirection = _destination - (Vector2)transform.position;
+        transform.position += (Vector3)movementDirection * MovementSpeed * Time.deltaTime;
+    }
+}
