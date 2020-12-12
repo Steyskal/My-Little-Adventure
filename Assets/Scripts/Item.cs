8138892d2412bdd88cfcfd9a90d18b4c32007388
Toni Steyskal <toni.steyskal@gmail.com>
2020-12-12 15:35:59 +0100

    Predavanja 12.12.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "My Little Adventure/Item")]
+public class Item : ScriptableObject
+{
+    public string Name;
+    public Sprite Icon;
+    public string Description;
+    public int Value;
+
+    public void Use()
+    {
+        Debug.Log(Description);
+    }
+}
